d9f59c41f2e9755f7eb0a9d5c15dcd41708ba440
AllHailMegatron <ahmadrthomp@gmail.com>
2019-08-07 21:36:10 -0400

    Created GET, POST, GET/USERS

--- index.js
+++ index.js
@@ -4,25 +4,64 @@ const express = require('express');
 const db = require('./data/db.js');
 
 const server = express();
+
 server.use(express.json());
 
-// POST /
-server.post('/db', (req, res) => {
-    const newDb = req.body;
-    db.add(newDb)
-    .then(db => {
-        res.status(400).json(db);
-    })
-    .catch(err => {
-        res.status(500).json({
-            err:err,
-            message: "Please provide name and bio for the user."
-        });
-    });
 
+// GET /
+server.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
 });
 
+server.get('/now', (req, res) => {
+    const now = new Date().toISOString()
+    res.send(now)
+  })
+  
 
+// POST /
+server.post('/api/users', (req, res) => {
+    const {name, bio} = req.body;
+    if ( !name || !bio) {
+        res.status(400).json({ 
+            err: err,
+            message: "Please provide name and bio for the user." })
+} else {
+        db.insert(req.body)
+    
+        .then(newUser => {
+            res.status(201).json(newUser);
+        })
+        .catch(err => {
+            res.status(500).json({
+                err:err,
+                message: "There was an error while saving the user to the database" 
+            });
+
+        });
+    }
+})
+
+
+// GET /users
+server.get('/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The users information could not be retrieved." 
+        })
+    })
+})
+
+// GET /:id
+server.get('/api/users/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(user => {
+
+    })
+})
 
 server.listen(5000, () =>{
     console.log('Server is running on port 5000...');
